59dd5d4bdf0174a277a8859d1a1a80d3b1e071dd
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2022-02-18 01:48:48 +0530

    info page design ad script

--- info.js
+++ info.js
@@ -14,43 +14,47 @@ function fetchRecipe() {
         // createFavItem(data);
         console.log("hello ", data.meals);
         data.meals.forEach((meal) => {
-          html += `
-          <!--<div class="image-container"> 
-          <img src="${meal.strMealThumb}" alt="image" id="rec-image">
-            </div> -->
-                
-                <div class="detail-container">
-                    <div class="common"><b>Food Name : </b> ${meal.strMeal}</div>
+          html += `<div class="details-conatiner">
+              <div class="meals-desc">
+              <h2 class="line"> ${meal.strMeal}</h2>
+              <p> Its a ${meal.strArea} ${meal.strCategory} Dish</p>
+              </div>
 
-                    <div class="common"><b>Food Category : </b> ${meal.strCategory}</div>
-                    <div> <img src="${meal.strMealThumb}" alt="image"></div>
-                   
-                    <div class="common"><b>Instructions : </b>${meal.strInstructions}</div>
-                    <div class="common">
-                        <b>Ingredients Required :</b>
-                        <div class="common">
-                        <ul>
-                            <li>${meal.strIngredient1}</li>
-                            <li>${meal.strIngredient2}</li>
-                            <li>${meal.strIngredient3}</li>
-                            <li>${meal.strIngredient4}</li>
-                            <li>${meal.strIngredient5}</li>
-                        </ul>
-                        </div>
-                    </div>
-                    <div class="common">
-                    <b>Watch : </b> For more details please watch on <a href="${meal.strYoutube}" target= "blank">Youtube</a>
-                    </div>
-                    
-                    
-                    
-                </div>
-            `;
+              <div class="mealsIng">
+
+               <h4>Required Indgredient for this dish</h4>
+               
+               <span>${meal.strIngredient1},${meal.strIngredient2},${meal.strIngredient3},${meal.strIngredient4},${meal.strIngredient5}</span>
+              </div>
+              <div class="mealImage">
+               <img src="${meal.strMealThumb}" alt="Meal Image"> 
+              </div>
+              
+
+
+               <div class="mealPrep" height:100vh width:70% ">
+              <h4>DIRECTIONS</h4>
+              
+                <p style="width:700px;height:500px ">${meal.strInstructions}e</p>
+  
+                <div>
+              <div class= "mealsFooter" style="width:500px;height:300px">
+              <p>
+              <h4>Watch Meal Video on <a href="${meal.strYoutube}" target= "blank">YOUTUBE</a></h1>
+              <h4>Give Shoutout to Creater and get similars recipes at  <a href="${meal.strSource}" target= "blank">HERE</a> </h2>
+              </p>
+            </div>
+              
+              
+
+
+            </div>`;
         });
         let recipecontainer = document.getElementById("info-container");
         recipecontainer.innerHTML = html;
       }
     });
 }
+
 
 fetchRecipe();
